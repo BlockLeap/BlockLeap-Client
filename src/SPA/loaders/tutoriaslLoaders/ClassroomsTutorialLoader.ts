cae57feeaf2bebc258d6566279628c4fcaae86cd
JieQiangG <jieqiang@ucm.es>
2025-04-23 17:30:18 +0200

    marked test

--- src/SPA/loaders/tutoriaslLoaders/ClassroomsTutorialLoader.ts
+++ src/SPA/loaders/tutoriaslLoaders/ClassroomsTutorialLoader.ts
@@ -1,6 +1,7 @@
 import { route } from "../../../client";
 import config from "../../../Game/config.js";
 import { fetchRequest, fillContent } from "../../utils";
+import {marked} from 'marked';
 
 const API_ENDPOINT = `${config.API_PROTOCOL}://${config.API_DOMAIN}:${config.API_PORT}/api`;
 
@@ -10,7 +11,7 @@ const API_ENDPOINT = `${config.API_PROTOCOL}://${config.API_DOMAIN}:${config.API
  */
 function getRowHTML() {
     return `
-      <div class="container bg-body rounded-3 mt-3 pb-3">
+      <div class="container bg-body rounded-3 mt-3 pb-3" id="tutorialContent">
           <h1 class="text-center w-75 mx-auto pt-3">Classrooms tutorial</h1>
         <p class="text-center w-75 mx-auto pt-3" >
           Once you enter the classroom tab, if you haven't signed in, you will need to do so in order to enjoy the benefits of being part of a class.<br><br>
@@ -23,10 +24,16 @@ function getRowHTML() {
 }
 
 
+
+
 export default async function classroomsTutorialLoader() {
   history.pushState({}, "", "/tutorials/classrooms");
   document.getElementById("content").innerHTML = getRowHTML();
   const divElement = document.getElementById("categories");
+  document.getElementById("tutorialContent").innerHTML = await marked.parse(`
+  # Markdown formatted text
+  This is **Markdown*`);
+
 
   try {
 
